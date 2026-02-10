2e5fcd7ba10f4c7fded07195b73c7e3e9eb36711
yug005 <yuga6627@gmail.com>
2026-02-10 11:25:32 +0530

    api response

---
+++ src/main/java/com/yug/bfhl/ApiResponse.java
@@ -0,0 +1,45 @@
+package com.yug.bfhl;
+
+public class ApiResponse {
+
+    private boolean is_success;
+    private String official_email = "yug2328.be23@chitkara.edu.in";
+    private Object data;
+    private String error;
+
+    public ApiResponse() {
+    }
+
+    public boolean isIs_success() {
+        return is_success;
+    }
+
+    public void setIs_success(boolean is_success) {
+        this.is_success = is_success;
+    }
+
+    public String getOfficial_email() {
+        return official_email;
+    }
+
+    public void setOfficial_email(String official_email) {
+        this.official_email = official_email;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+}
---
+++ src/main/java/com/yug/bfhl/BfhlApplication.java
@@ -0,0 +1,13 @@
+package com.yug.bfhl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfhlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BfhlApplication.class, args);
+    }
+
+}
